bb6309a208a3d7406f479e254b7e2ac603af235f
Satyam kannojia <satyamkan17@gmail.com>
2024-04-21 22:19:59 +0530

    Add files via upload

---
+++ satyam.js
@@ -0,0 +1,40 @@
+let a = parseFloat(prompt("enter a first number"));
+let b = parseFloat(prompt("enter a second number"));
+let c = prompt("enter the type of calculation +,-,*,/");
+
+if (Math.random() < 0.1) {
+    if (c == '+') {
+        const fault1 = a - b;
+        document.write(fault1);
+    }
+    else if (c == '-') {
+        let fault2 = a / b;
+        document.write(fault2);
+    }
+    else if (c == '*') {
+        let fault3 = a + b;
+        document.write(fault3);
+    }
+    else if (c == '/') {
+        let fault4 = a ** b
+        document.write(fault4);
+    }
+}
+else {
+    if (c == '+'){
+        let add = a + b;
+        document.write(add);
+    }
+    else if (c == '-') {
+        let minus = a - b;
+        document.write((minus));
+    }
+    else if (c == '*') {
+        let multi = a * b;
+        document.write(multi);
+    }
+    else if (c == '/') {
+        let divide = a / b;
+        document.write(divide);
+    }
+}
