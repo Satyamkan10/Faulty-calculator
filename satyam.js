840d26a88ed99fb1902168c8c2953c4a43a3de7a
Satyam kannojia <satyamkan17@gmail.com>
2024-04-25 17:14:34 +0530

    Update satyam.js

--- satyam.js
+++ satyam.js
@@ -5,39 +5,36 @@ while (repeat) {
     let b = parseFloat(prompt("Enter the second number"));
     let c = prompt("Enter the type of calculation (+, -, *, /)");
 
+    let result;
+
     if (Math.random() < 0.1) {
         // Fault
         if (c == '+') {
-            const fault1 = a - b;
-            document.write(fault1);
+            result = a - b;
         } else if (c == '-') {
-            let fault2 = a / b;
-            document.write(fault2);
+            result = a / b;
         } else if (c == '*') {
-            let fault3 = a + b;
-            document.write(fault3);
+            result = a + b;
         } else if (c == '/') {
-            let fault4 = a ** b;
-            document.write(fault4);
+            result = a ** b;
         }
     } else {
         // Correct
         if (c == '+') {
-            let add = a + b;
-            document.write(add);
+            result = a + b;
         } else if (c == '-') {
-            let minus = a - b;
-            document.write(minus);
+            result = a - b;
         } else if (c == '*') {
-            let multi = a * b;
-            document.write(multi);
+            result = a * b;
         } else if (c == '/') {
-            let divide = a / b;
-            document.write(divide);
+            result = a / b;
         }
     }
 
-    // Ask the user if they want to repeat
+    // Display the result
+    document.write("Result: " + result + "<br>");
+
+    // Ask the user if they want to perform another calculation
     let response = prompt("Do you want to perform another calculation? (yes/no)");
     if (response.toLowerCase() !== "yes") {
         repeat = false; // Exit the loop if the user does not want to repeat
