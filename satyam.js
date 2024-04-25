9b153ea501138dbf39c6ee7d28fae6cbaf6086af
Satyam kannojia <satyamkan17@gmail.com>
2024-04-25 17:17:06 +0530

    Update satyam.js

--- satyam.js
+++ satyam.js
@@ -1,42 +1,42 @@
-let repeat = true;
+ let a = parseFloat(prompt("enter a first number"));
+let b = parseFloat(prompt("enter a second number"));
+let c = prompt("enter the type of calculation +,-,*,/");
 
-while (repeat) {
-    let a = parseFloat(prompt("Enter the first number"));
-    let b = parseFloat(prompt("Enter the second number"));
-    let c = prompt("Enter the type of calculation (+, -, *, /)");
-
-    let result;
-
-    if (Math.random() < 0.1) {
-        // Fault
-        if (c == '+') {
-            result = a - b;
-        } else if (c == '-') {
-            result = a / b;
-        } else if (c == '*') {
-            result = a + b;
-        } else if (c == '/') {
-            result = a ** b;
-        }
-    } else {
-        // Correct
-        if (c == '+') {
-            result = a + b;
-        } else if (c == '-') {
-            result = a - b;
-        } else if (c == '*') {
-            result = a * b;
-        } else if (c == '/') {
-            result = a / b;
-        }
+if (Math.random() < 0.1) {
+    // fault 
+    if (c == '+') {
+        const fault1 = a - b;
+        document.write(fault1);
     }
-
-    // Display the result
-    document.write("Result: " + result + "<br>");
-
-    // Ask the user if they want to perform another calculation
-    let response = prompt("Do you want to perform another calculation? (yes/no)");
-    if (response.toLowerCase() !== "yes") {
-        repeat = false; // Exit the loop if the user does not want to repeat
+    else if (c == '-') {
+        let fault2 = a / b;
+        document.write(fault2);
+    }
+    else if (c == '*') {
+        let fault3 = a + b;
+        document.write(fault3);
+    }
+    else if (c == '/') {
+        let fault4 = a ** b
+        document.write(fault4);
+    }
+}
+else {
+    // correct
+    if (c == '+'){
+        let add = a + b;
+        document.write(add);
+    }
+    else if (c == '-') {
+        let minus = a - b;
+        document.write((minus));
+    }
+    else if (c == '*') {
+        let multi = a * b;
+        document.write(multi);
+    }
+    else if (c == '/') {
+        let divide = a / b;
+        document.write(divide);
     }
 }
